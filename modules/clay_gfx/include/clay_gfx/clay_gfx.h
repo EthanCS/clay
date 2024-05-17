ead6b333d0ce279ae9dfb5034e0bd9351e032f10
DESKTOP_CY01\ethan <ethancs1993@gmail.com>
2024-05-17 23:17:55 +0800

    add flecs and some basic handle

--- modules/clay_gfx/include/clay_gfx/clay_gfx.h
+++ modules/clay_gfx/include/clay_gfx/clay_gfx.h
@@ -1,9 +1,24 @@
 #pragma once
 
+#include <flecs.h>
+
+#define DEFINE_CLAY_GFX_HANDLE(name) \
+    struct name##Handle {            \
+        flecs::entity entity;        \
+    };
+
 namespace clay
 {
 namespace gfx
 {
+
+DEFINE_CLAY_GFX_HANDLE(Buffer)
+DEFINE_CLAY_GFX_HANDLE(Texture)
+DEFINE_CLAY_GFX_HANDLE(Sampler)
+DEFINE_CLAY_GFX_HANDLE(Shader)
+DEFINE_CLAY_GFX_HANDLE(Pipeline)
+DEFINE_CLAY_GFX_HANDLE(RenderPass)
+DEFINE_CLAY_GFX_HANDLE(Framebuffer)
 
 struct BackendType {
     enum Enum
--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,8 @@
 set_languages("c99", "cxx20")
 add_rules("mode.debug", "mode.release")
 
+add_requires("flecs 3.2.11")
+
 includes("modules")
 
 target("test")
