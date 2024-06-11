33bf457f7e49664ea8e196713943f49c229838a3
EthanCS <ethancs1993@gmail.com>
2024-06-11 10:39:19 +0800

    quickjs now use custom repo

--- xmake.lua
+++ xmake.lua
@@ -2,8 +2,10 @@ set_languages("c99", "cxx20")
 add_rules("mode.debug", "mode.release")
 add_rules("plugin.compile_commands.autoupdate", { outputdir = ".vscode" })
 
+add_repositories("clay-xrepo xrepo", {rootdir = os.scriptdir()})
+
 add_requires("vulkansdk", "mimalloc", "spdlog", "libsdl 2.30.3")
-add_requires("quickjs")
+add_requires("quickjs-clay 1.0.0")
 
 includes("thirdparty")
 includes("modules")
