cd2c07f0cfa6aab015237e03d5a09a2b833cd4a9
EthanCS <ethancs1993@gmail.com>
2024-06-19 20:28:18 +0800

    try daScript

---
+++ xrepo/packages/d/daScript/xmake.lua
@@ -0,0 +1,75 @@
+package("daScriptCore")
+    set_homepage("https://dascript.org/")
+    set_description("daScript - high-performance statically strong typed scripting language")
+    set_license("BSD-3-Clause")
+
+    add_urls("https://github.com/GaijinEntertainment/daScript.git")
+    add_versions("0.4.9", "2ded4ac42970171b14aff6b28ffb974fc10a18f0")
+
+    add_deps("cmake")
+
+    on_install(function (package)
+        local configs = {
+            -- "-DDAS_AOT_EXAMPLES_DISABLED=ON",
+            -- "-DDAS_PROFILE_DISABLED=ON",
+            -- "-DDAS_TUTORIAL_DISABLED=ON",
+            -- "-DDAS_TESTS_DISABLED=ON",
+        }
+        table.insert(configs, "-DCMAKE_BUILD_TYPE=" .. (package:debug() and "Debug" or "Release"))
+        table.insert(configs, "-DBUILD_SHARED_LIBS=" .. (package:config("shared") and "ON" or "OFF"))
+
+        import("package.tools.cmake").build(package, configs, {buildir = "build"})
+
+        if package:is_plat("windows") then
+                os.trycp("build/**.dll", package:installdir("bin"))
+                os.trycp("build/**.lib", package:installdir("lib"))
+        elseif package:is_plat("mingw") then
+                os.trycp("build/**.dll", package:installdir("bin"))
+                os.trycp("build/**.a", package:installdir("lib"))
+        else
+                os.trycp("build/*.so", package:installdir("bin"))
+                os.trycp("build/*.so", package:installdir("lib"))
+                os.trycp("build/*.a", package:installdir("lib"))
+        end
+        
+        os.cp("include", package:installdir())
+    end)
+
+    on_test(function (package)
+        assert(package:check_cxxsnippets({test = [[
+            #include "daScript/daScript.h"
+            using namespace das;
+            const char * tutorial_text = R""""(
+            [export]
+            def test
+                print("this is nano tutorial\n")
+            )"""";
+            static void test() 
+            {
+                // request all da-script built in modules
+                NEED_ALL_DEFAULT_MODULES;
+                // Initialize modules
+                Module::Initialize();
+                // make file access, introduce string as if it was a file
+                auto fAccess = make_smart<FsFileAccess>();
+                auto fileInfo = make_unique<TextFileInfo>(tutorial_text, uint32_t(strlen(tutorial_text)), false);
+                fAccess->setFileInfo("dummy.das", das::move(fileInfo));
+                // compile script
+                TextPrinter tout;
+                ModuleGroup dummyLibGroup;
+                auto program = compileDaScript("dummy.das", fAccess, tout, dummyLibGroup);
+                if ( program->failed() ) return;
+                // create context
+                Context ctx(program->getContextStackSize());
+                if ( !program->simulate(ctx, tout) ) return;
+                // find function. its up to application to check, if function is not null
+                auto function = ctx.findFunction("test");
+                if ( !function ) return;
+                // call context function
+                ctx.evalWithCatch(function, nullptr);
+                // shut-down daScript, free all memory
+                Module::Shutdown();
+            }
+        ]]}, {configs = {languages = "c++17"}}))
+    end)
+package_end()
